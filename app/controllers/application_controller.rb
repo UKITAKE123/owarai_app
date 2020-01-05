1f2a292e3121cdc4f01ad7d20b9d4608ebb63ee7
takehiro <uktk.1022.zz123@outlook.jp>
2020-01-05 16:17:25 +0900

    add basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception #これ何のコードなんだろうか
+  include SessionsHelper #ここに入れることでどのcontrollerでも使えるようになる
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "お笑いどんへようこそ!"
       redirect_to @user
     else
@@ -23,4 +24,3 @@ private
   def user_params
     params.require(:user).permit(:name,  :email,  :password,  :password_confirmation)
   end
-end
